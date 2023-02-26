79985ec8478a4381ff92b9af15eb09e67e4528e0
Deeptansh Verma <75324676+DeeptanshV@users.noreply.github.com>
2023-02-26 18:57:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const button=document.querySelector("button");
 const h1=document.querySelector("h1");
 button.addEventListener("click",function(){
- 
+
     const r=Math.floor(Math.random()*256);
     const g=Math.floor(Math.random()*256);
     const b=Math.floor(Math.random()*256);
     let newColor=`rgb(${r},${g},${b})`;
     document.body.style.backgroundColor=newColor;
-   
+    h1.style.color= "white";
     document.querySelector("h1").innerText=newColor;
 })
 
